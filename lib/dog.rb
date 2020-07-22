a592dba0fe4a74c51ea74d587c9d5a0fafe67090
Desiree Marquez <marquezdecardona@gmail.com>
2020-07-22 17:05:35 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,18 @@
-class Dog 
-  def initialize (name, breed)
-    @name= name 
+class Dog
+  def initialize (dogs_name, dogs_breed = "Mutt")
+    @name = dogs_name
+    @breed = dogs_breed
   end
-    
-  def name 
+
+  def name=(dogs_name, dogs_breed = "Mutt")
+    @name = dogs_name
+    @breed = dogs_breed
+  end
+
+  def name
     @name
   end
-  
-  def initialize
-end
+  def breed
+    @breed
+  end 
+end 
